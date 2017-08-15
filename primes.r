ed51ed649cca0d4f4aa14dcd6e31074678757d79
Adam Clark <Autoboticon@gmail.com>
2017-08-15 17:10:13 +0100

    primes 1 task

---
+++ primes.r
@@ -0,0 +1,11 @@
+primest <- function(n){
+  p <- 2:n
+  i <- 1
+  while (p[i] <= sqrt(n)) {
+    p <-  p[p %% p[i] != 0 | p==p[i]]
+    i <- i+1
+  }
+  p
+}
+primes = primest(3000000)
+print(length(primes))
