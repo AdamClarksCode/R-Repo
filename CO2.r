e077cf4766f9e5e6561ba8613f7a3d2e5dd06e81
Adam Clark <Autoboticon@gmail.com>
2017-08-15 13:55:37 +0100

    Finished CO2 Task

--- CO2.r
+++ CO2.r
@@ -1,5 +1,23 @@
 data(CO2)
-print(CO2)
 str(CO2) #Ord.factor
 mean_uptake = mean(CO2[["uptake"]])
 print(mean_uptake)
+boxplot(uptake~Type, data=CO2)
+quebec_CO2 <- subset(CO2, Type=="Quebec")
+print(quebec_CO2)
+mississippi_CO2 <- subset(CO2, Type=="Mississippi")
+print(mississippi_CO2)
+calMean <- function(set1, set2){
+  mean1 <- mean(set1[["uptake"]])
+  mean2 <- mean(set2[["uptake"]])
+  if(mean1 > mean2) {
+    print(cat("Set 1 has a higher mean of", mean1))
+    print(cat("Set 2 has a mean of", mean2))
+  } else if(mean2 > mean1){
+    print(cat("Set 2 has a higher mean of", mean2))
+    print(cat("Set 1 has a mean of", mean1))
+  } else{
+    print(cat("Set 1 and 2 have the same mean of ", mean1))
+  }
+}
+calMean(quebec_CO2, mississippi_CO2)
