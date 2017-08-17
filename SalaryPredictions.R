e5a444288a7c8dd66180188bb7facd57329e2af3
Adam Clark <Autoboticon@gmail.com>
2017-08-17 10:29:29 +0100

    Correct prediction

--- SalaryPredictions.R
+++ SalaryPredictions.R
@@ -1,17 +1,15 @@
 library(rpart)
-#install.packages(('rattle'))
-#install.packages('rpart.plot')
-#install.packages('RColorBrewer')
 library(rattle)
 library(rpart.plot)
 library(RColorBrewer)
 setwd("C:/Users/Administrator/Documents/r/Salary_data")
-CensusData <- read.csv("censusData_train.csv", header<-F, names = c("Age", "Workclass", "FinalWeight", "Education", 
-                                                                    "Education", "EducationNum", "MaritalStatus", 
-                                                                    "Occupation", "Relationship", "Race", "Sex",
-                                                                    "CapitalGain", "CapitalLoss", "HoursPerWeek",
-                                                                    "NatuveCountry", "Salary"))
-toGet <- "Private"
-PrivateWorkers <- subset(CensusData, Workclass == " Private")
-fit <- rpart(Salary ~ Education + EducationNum + Age, data=PrivateWorkers, method='class')
-print(PrivateWorkers)
+CensusData <- read.csv("censusData_train.csv", header<-F, stringsAsFactors=F) 
+names(CensusData) = c("Age", "Workclass", "FinalWeight", "Education", 
+                      "EducationNum", "MaritalStatus", 
+                      "Occupation", "Relationship", "Race", "Sex",
+                      "CapitalGain", "CapitalLoss", "HoursPerWeek",
+                      "NativeCountry", "Salary")
+CensusData$CapitalGain[CensusData$CapitalGain < 5119] <- 0
+CensusData$CapitalGain[CensusData$CapitalGain >= 5119] <- 1
+fit <- rpart(Salary ~ CapitalGain + Education + CapitalLoss, data=CensusData, method='class')
+fancyRpartPlot(fit)
